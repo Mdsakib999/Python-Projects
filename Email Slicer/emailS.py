40fdd3b7847f6bafca1d5c904a40a51f04e9481d
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-10-26 22:23:55 +0600

    Add files via upload

---
+++ Email Slicer/emailS.py
@@ -0,0 +1,11 @@
+
+
+email_id = input("Enter your Email: ").strip()
+
+user_name = email_id[:email_id.index("@")]
+
+domain_name = email_id[email_id.index("@")+1:]
+
+result = (f"Your name is '{user_name}' and your domain name is '{domain_name}'")
+
+print(result)
